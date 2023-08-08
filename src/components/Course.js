55950e4fcdd4de7232afdf14c9e7e45b1871207d
Sourabh Bindal <95605311+bindalsourabh1@users.noreply.github.com>
2023-08-08 13:56:45 +0530

    commented_code

--- src/components/Course.js
+++ src/components/Course.js
@@ -6,9 +6,9 @@ function Course({ item }) {
   return (
     <div className='flex flex-col items-start space-y-[1px]'>
       <img src={img1} alt="" className='h-32 w-full' />
-      <h2 className='font-bold text-md pt-1 '>
+      {/* <h2 className='font-bold text-md pt-1 '>
         {item.title}
-      </h2>
+      </h2> */}
     </div>
   );
 }
